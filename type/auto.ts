486c1e4f3eb709d070279319f580541747766edc
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-22 02:49:21 -0400

    Add condition for manual form upload

--- type/auto.ts
+++ type/auto.ts
@@ -1,5 +1,28 @@
-export type createNewJobType = {
+export type CreateNewJobType = {
   jobLink: string;
-  position: number;
   interviewDate: Date;
+  type: "link" | "manual";
+  manualForm: FormDataType;
+};
+
+export type FormDataType = {
+  title: string;
+  company: string;
+  location: string;
+  platform: string;
+  link: string;
+  description: string;
+};
+
+export type JobDataType = {
+  title: string;
+  link: string;
+  company: string;
+  location: string;
+  description: string;
+  logo?: string;
+  platformJobId: string | undefined;
+  platform: string;
+  summary: string;
+  skills: [];
 };
