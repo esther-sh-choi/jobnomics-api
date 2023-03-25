56a8b0ef47d9f34482a1fdb7dbe3c8da26fb7130
Viet Tran <70352144+tienviet10@users.noreply.github.com>
2023-03-24 20:53:05 -0400

    Delete build directory since it is not needed

--- build/routes/auto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-const { createNewJob } = require("../controller/auto-controller");
-const { getUserInfo } = require('../helper/auth');
-// Switched to get to check if puppeteer works
-router.post("/", getUserInfo, createNewJob);
-module.exports = router;
--- build/server.d.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { Server } from "socket.io";
-export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import(".prisma/client").Prisma.RejectOnNotFound | import(".prisma/client").Prisma.RejectPerOperation | undefined>;
-export declare const io: Server<import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, any>;
